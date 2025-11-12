42b91c937defb9399543ff2f2eded9d6397fd464
aricandra3 <rahmanagaari085@gmail.com>
2025-11-12 21:50:21 +0700

    update herosection

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -1,86 +1,93 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 import Style from './HeroSection.module.css';
 import images from '../../assets';
-import {TokenList, SearchToken} from '../index';
+import { TokenList, SearchToken } from '../index';
 
-const HeroSection = ({ accounts, tokenData}) => {
-  // USESTATE
+const HeroSection = ({ accounts, tokenData }) => {
+  // STATE
   const [openSetting, setOpenSetting] = useState(false);
-  const [openToken, setOpenToken] = useState(false);
-  const [openTokensTwo, setOpenTokensTwo] = useState(false);
+  const [openTokenOne, setOpenTokenOne] = useState(false);
+  const [openTokenTwo, setOpenTokenTwo] = useState(false);
 
-  // Token 1
-const [tokenOne, setTokenOne] = useState({
-  name: "",
-  image: "",
-});
+  const [tokenOne, setTokenOne] = useState({ name: '', image: '' });
+  const [tokenTwo, setTokenTwo] = useState({ name: '', image: '' });
 
-  // Token 2
-const [tokenTwo, setTokenTwo] = useState({
-  name: "",
-  image: "",
-});
-
-// /JSX
   return (
     <div className={Style.HeroSection}>
       <div className={Style.HeroSection_box}>
         <div className={Style.HeroSection_box_heading}>
           <p>Swap</p>
           <div className={Style.HeroSection_box_heading_img}>
-            <Image src={images.close} alt="image" width={50} height={50}
-            onClick={() => setOpenSetting(true)}
+            <Image
+              src={images.close}
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => setOpenSetting(true)}
             />
           </div>
         </div>
 
+        {/* INPUT 1 */}
         <div className={Style.HeroSection_box_input}>
           <input type="text" placeholder="0" />
-          <button onClick={()=> openToken(true)}>
-            <Image src={images.image || images.etherlogo} width={20} height={20} alt="ether"
+          <button onClick={() => setOpenTokenOne(true)}>
+            <Image
+              src={tokenOne.image || images.etherlogo}
+              width={20}
+              height={20}
+              alt="token-1"
             />
-            {tokenOne.name || "ETH"}
+            {tokenOne.name || 'ETH'}
             <small>9474</small>
           </button>
         </div>
 
+        {/* INPUT 2 */}
         <div className={Style.HeroSection_box_input}>
           <input type="text" placeholder="0" />
-          <button onClick={()=> openToken(true)}>
-            <Image src={tokenTwo.image || images.etherlogo} width={20} height={20} alt="ether"
+          <button onClick={() => setOpenTokenTwo(true)}>
+            <Image
+              src={tokenTwo.image || images.etherlogo}
+              width={20}
+              height={20}
+              alt="token-2"
             />
-            {tokenTwo.name || "ETH"}
+            {tokenTwo.name || 'ETH'}
             <small>9474</small>
           </button>
         </div>
-        {accounts ?(
+
+        {accounts ? (
+          <button className={Style.HeroSection_box_btn}>Swap</button>
+        ) : (
           <button className={Style.HeroSection_box_btn}>Connect Wallet</button>
-        ): (
-          <button className={Style.HeroSection_box_btn}
-          onClick={()=>{}}>Swap</button>
         )}
       </div>
 
-      {openSetting && <Token openSetting={openSetting}/>}
+      {openSetting && <TokenList setOpenTokenbox={setOpenSetting} />}
 
-      {openToken && (
+      {/* Search modal untuk Token 1 */}
+      {openTokenOne && (
         <SearchToken
-          openToken={setOpenToken}
-          tokens={setTokenOne}
+          setOpenToken={setOpenTokenOne} 
+          setToken={setTokenOne}          
           tokenData={tokenData}
         />
       )}
-        {openToken && (
+
+      {/* Search modal untuk Token 2 */}
+      {openTokenTwo && (
         <SearchToken
-          openToken={setOpenTokensTwo}
-          tokens={setTokenTwo}
+          setOpenToken={setOpenTokenTwo}
+          setToken={setTokenTwo}
           tokenData={tokenData}
         />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
