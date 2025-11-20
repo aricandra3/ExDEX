91d2766a062daebaf500668fb2ae803ace5a7c82
aricandra3 <rahmanagaari085@gmail.com>
2025-11-20 22:55:09 +0700

    token update 1

--- Components/Token/Token.jsx
+++ Components/Token/Token.jsx
@@ -11,7 +11,11 @@ const Token = ({ openSetting }) => {
     <div className={Style.Token}>
         <div className={Style.Token_box}>
             <div className={Style.Token_box_heading}>
-                <h4>Setting</h4> 
+                <h4>Setting</h4>
+                <Image src={images.close}
+                alt='close'
+                width={50}
+                height={50}/> 
             </div>
         </div>
     </div>
